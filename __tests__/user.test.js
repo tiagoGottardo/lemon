b32d40bc297a8ea16612424e0617b335cda34a7e
tiagoGott <tiago.panizio@hotmail.com>
2023-10-12 17:08:26 -0300

    added get endpoint tests.

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,6 +1,9 @@
 const request = require('supertest')
 const { app } = require('../server')
 const jwt = require("jsonwebtoken")
+const db = require("../app/models")
+const User = db.user
+
 require('dotenv').config()
 
 describe("Get /api/test/all", () => {
@@ -43,7 +46,9 @@ describe("Get /api/test/user", () => {
   })
 
   it("should return user content, status 200.", async () => {
-    token = jwt.sign({ id: "6525ec5ea937ea3b1f3bd18c" }, process.env.SECRET, {
+    var user = await User.findOne({ username: "userRole" }).exec()
+    
+    token = jwt.sign({ id: user._id }, process.env.SECRET, {
       algorithm: 'HS256',
       allowInsecureKeySizes: true,
       expiresIn: 86400, // 24 hours
@@ -65,10 +70,128 @@ describe("Get /api/test/mod", () => {
       expiresIn: 86400, // 24 hours
     })
     const response = await request(app)
-    .get("/api/test/user")
+    .get("/api/test/mod")
     .set('x-access-token', token)
     expect(response.statusCode).toBe(404)
     expect(response.body).toEqual({ message: "User not found" })
+  })
+
+  it("should return user has no roles, status 403.", async () => {
+    user = await User.findOne({ username: "tiagovsk" }).exec()
+    
+    token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      algorithm: 'HS256',
+      allowInsecureKeySizes: true,
+      expiresIn: 86400, // 24 hours
+    })
+    const response = await request(app)
+    .get("/api/test/mod")
+    .set('x-access-token', token)
+    expect(response.statusCode).toBe(403)
+    expect(response.body).toEqual({ message: "User has no roles" })
+  })
+
+
+  it("should return require mod role, status 403.", async () => {
+    user = await User.findOne({ username: "userRole" }).exec()
+    token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      algorithm: 'HS256',
+      allowInsecureKeySizes: true,
+      expiresIn: 86400, // 24 hours
+    })
+    const response = await request(app)
+    .get("/api/test/mod")
+    .set('x-access-token', token)
+    expect(response.statusCode).toBe(403)
+    expect(response.body).toEqual({ message: "Require Moderator Role!" })
+  })
+
+
+  it("should return require mod role, status 403.", async () => {
+    user = await User.findOne({ username: "userAdminRole" }).exec()
+    token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      algorithm: 'HS256',
+      allowInsecureKeySizes: true,
+      expiresIn: 86400, // 24 hours
+    })
+    const response = await request(app)
+    .get("/api/test/mod")
+    .set('x-access-token', token)
+    expect(response.statusCode).toBe(403)
+    expect(response.body).toEqual({ message: "Require Moderator Role!" })
+  })
+
+  it("should return mod content, status 200.", async () => {
+    user = await User.findOne({ username: "userModRole" }).exec()
+    token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      algorithm: 'HS256',
+      allowInsecureKeySizes: true,
+      expiresIn: 86400, // 24 hours
+    })
+    const response = await request(app)
+    .get("/api/test/mod")
+    .set('x-access-token', token)
+    expect(response.statusCode).toBe(200)
+    expect("Moderator Content.")
+  })
+
+})
+
+describe("Get /api/test/admin", () => {
+  it("should return user not found, status 404.", async () => {
+    token = jwt.sign({ id: "6527519717979ccc42ec6a1c" }, process.env.SECRET, {
+      algorithm: 'HS256',
+      allowInsecureKeySizes: true,
+      expiresIn: 86400, // 24 hours
+    })
+    const response = await request(app)
+    .get("/api/test/admin")
+    .set('x-access-token', token)
+    expect(response.statusCode).toBe(404)
+    expect(response.body).toEqual({ message: "User not found" })
+  })
+
+  it("should return user has no roles, status 403.", async () => {
+    user = await User.findOne({ username: "tiagovsk" }).exec()
+    token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      algorithm: 'HS256',
+      allowInsecureKeySizes: true,
+      expiresIn: 86400, // 24 hours
+    })
+    const response = await request(app)
+    .get("/api/test/admin")
+    .set('x-access-token', token)
+    expect(response.statusCode).toBe(403)
+    expect(response.body).toEqual({ message: "User has no roles" })
+  })
+
+
+  it("should return require admin role, status 403.", async () => {
+    user = await User.findOne({ username: "userModRole" }).exec()
+    token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      algorithm: 'HS256',
+      allowInsecureKeySizes: true,
+      expiresIn: 86400, // 24 hours
+    })
+    const response = await request(app)
+    .get("/api/test/admin")
+    .set('x-access-token', token)
+    expect(response.statusCode).toBe(403)
+    expect(response.body).toEqual({ message: "Require Admin Role!" })
+  })
+
+  it("should return admin content, status 200.", async () => {
+    user = await User.findOne({ username: "userAdminRole" }).exec()
+    token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      algorithm: 'HS256',
+      allowInsecureKeySizes: true,
+      expiresIn: 86400, // 24 hours
+    })
+    const response = await request(app)
+    .get("/api/test/admin")
+    .set('x-access-token', token)
+    expect(response.statusCode).toBe(200)
+    expect("Admin Content.")
   })
 
 })
